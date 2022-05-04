21a2fe6daabf6a37467e3987a3bc354bc8f70169
Daniil <daniil.postoyuk@gmail.com>
2022-05-04 16:37:50 +0300

    all finish

--- src/js/script.js
+++ src/js/script.js
@@ -131,4 +131,23 @@ $(document).ready(function(){
 		});
 		return false;
 	});
+
+	//smooth scroll and pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else $('.pageup').fadeOut();
+	});
+
+	$("a[href=#]").click(function() {
+		const _href = $(this).attr("href");
+		$('html,body').animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
+	//plagin wow
+
+	new WOW().init();
+
 });
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,14 +11,14 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  						// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'daniil.postoyuk';                 // Наш логин
-$mail->Password = 'vzefyzjrxhvtlnba';                           // Наш пароль от ящика
+$mail->Username = 'daniil.postoyuk@gmail.com';         // Наш логин
+$mail->Password = 'uirepmkiqfokqsie';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('daniil.postoyuk@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('example@gmail.com', 'Pulse');  		 // От кого письмо 
 $mail->addAddress('daniil.postoyuk@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
